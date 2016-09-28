a363ea4095361542948c7329116d50762e431fe7
david sarginson <david.sarginson@opencastsoftware.com>
2016-09-28 15:04:05 +0100

    initial project commit

---
+++ app/uk/gov/hmrc/trustregistration/apiGlobal.scala
@@ -0,0 +1,85 @@
+/*
+ * Copyright 2016 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.trustregistration
+
+import com.typesafe.config.Config
+import play.api._
+import uk.gov.hmrc.api.connector.ServiceLocatorConnector
+import uk.gov.hmrc.play.audit.filters.AuditFilter
+import uk.gov.hmrc.play.auth.controllers.AuthParamsControllerConfig
+import uk.gov.hmrc.play.config.{AppName, ControllerConfig, RunMode}
+import uk.gov.hmrc.play.http.{HttpPost, HeaderCarrier}
+import uk.gov.hmrc.play.http.logging.filters.LoggingFilter
+import uk.gov.hmrc.play.microservice.bootstrap.DefaultMicroserviceGlobal
+import uk.gov.hmrc.play.auth.microservice.filters.AuthorisationFilter
+import net.ceedubs.ficus.Ficus._
+import scala.concurrent.ExecutionContext.Implicits.global
+
+
+trait ServiceLocatorRegistration extends GlobalSettings with RunMode {
+
+  val registrationEnabled: Boolean
+  implicit val hc: HeaderCarrier
+
+  override def onStart(app: Application): Unit = {
+    Logger.debug("Registering with service-locator")
+    ServiceLocatorConnector(WSHttp).register map { result =>
+      Logger.debug(s"service-locator registraion result: $result")
+    }
+  }
+}
+
+
+object ControllerConfiguration extends ControllerConfig {
+  lazy val controllerConfigs = Play.current.configuration.underlying.as[Config]("controllers")
+}
+
+object AuthParamsControllerConfiguration extends AuthParamsControllerConfig {
+  lazy val controllerConfigs = ControllerConfiguration.controllerConfigs
+}
+
+object ApiAuditFilter extends AuditFilter with AppName {
+  override val auditConnector = MicroserviceAuditConnector
+  override def controllerNeedsAuditing(controllerName: String) = ControllerConfiguration.paramsForController(controllerName).needsAuditing
+}
+
+object ApiLoggingFilter extends LoggingFilter {
+  override def controllerNeedsLogging(controllerName: String) = ControllerConfiguration.paramsForController(controllerName).needsLogging
+}
+
+object ApiAuthFilter extends AuthorisationFilter {
+  override lazy val authParamsConfig = AuthParamsControllerConfiguration
+  override lazy val authConnector = MicroserviceAuthConnector
+  override def controllerNeedsAuth(controllerName: String): Boolean = ControllerConfiguration.paramsForController(controllerName).needsAuth
+}
+
+object ApiGlobal extends DefaultMicroserviceGlobal with RunMode with ServiceLocatorRegistration {
+
+  override val registrationEnabled = true
+
+  override val auditConnector = MicroserviceAuditConnector
+
+  override def microserviceMetricsConfig(implicit app: Application): Option[Configuration] = app.configuration.getConfig(s"microservice.metrics")
+
+  override val loggingFilter = ApiLoggingFilter
+
+  override val microserviceAuditFilter = ApiAuditFilter
+
+  override val authFilter = Some(ApiAuthFilter)
+
+  override implicit val hc: HeaderCarrier = HeaderCarrier()
+}
