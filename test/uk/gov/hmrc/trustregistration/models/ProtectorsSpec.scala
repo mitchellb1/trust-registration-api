a2f0f59243db27744c49c2441f6762abd2625cfd
Chris Waugh <mail@chriswaugh.net>
2016-11-11 11:08:01 +0000

    Created Protectors model, reads, writes and unit tests

---
+++ test/uk/gov/hmrc/trustregistration/models/ProtectorsSpec.scala
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2016 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.trustregistration.models
+
+import org.scalatestplus.play.PlaySpec
+import play.api.libs.json.Json
+import uk.gov.hmrc.trustregistration.{JsonExamples, ScalaDataExamples}
+
+class ProtectorsSpec extends PlaySpec with JsonExamples with ScalaDataExamples {
+
+  "Protectors" must {
+    "serialize to json" in {
+      val protectors = new Protectors(individuals = Some(List(individual)), companies = Some(List(company)))
+      val details = Json.toJson[Protectors](protectors).toString()
+      details must include ("Spaceman")
+    }
+    "output a none value as an empty array for individuals and companies" in {
+      val protectors = new Protectors(None, None)
+      val details = Json.toJson[Protectors](protectors).toString()
+      details must be ("""{"individuals":[],"companies":[]}""")
+    }
+  }
+
+}
