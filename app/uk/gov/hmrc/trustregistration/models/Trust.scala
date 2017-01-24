a60a02026b9620d0a8d8f59c850e4f61ac0a44d8
David Wright <davew@MacBook-Pro.local>
2017-01-24 13:35:48 +0000

    Existing tests pass

--- app/uk/gov/hmrc/trustregistration/models/Trust.scala
+++ app/uk/gov/hmrc/trustregistration/models/Trust.scala
@@ -19,7 +19,6 @@ package uk.gov.hmrc.trustregistration.models
 import org.joda.time.DateTime
 import play.api.libs.json.{JsString, Json, Reads, Writes}
 
-
 case class Trust(name: String,
                  correspondenceAddress: Address,
                  telephoneNumber: String,
@@ -33,7 +32,7 @@ case class Trust(name: String,
                  protectors: Protectors,
                  settlors: Settlors,
                  naturalPeople: Option[NaturalPeople] = None,
-                 trustTypeType: TrustType,
+                 trustType: TrustType,
                  declaration: Option[Declaration] = None,
                  isSchedule5A: Option[Boolean] = None,
                  nonResidentType: Option[String] = None
