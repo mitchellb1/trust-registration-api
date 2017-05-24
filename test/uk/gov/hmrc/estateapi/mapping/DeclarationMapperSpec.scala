32a21def5bc58128be7ae19dc0a02f4de4ef9192
juan <juanan_rp@hotmail.co.uk>
2017-05-24 10:00:41 +0100

    starting mapping declartion properties

---
+++ test/uk/gov/hmrc/estateapi/mapping/DeclarationMapperSpec.scala
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2017 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.estateapi.mapping
+
+import org.joda.time.DateTime
+import org.scalatestplus.play.{OneAppPerSuite, PlaySpec}
+import uk.gov.hmrc.common.des.DesDeclaration
+import uk.gov.hmrc.common.mapping.AddressMapper
+import uk.gov.hmrc.common.rest.resources.core.Declaration
+import uk.gov.hmrc.utils.{DesScalaExamples, ScalaDataExamples}
+
+
+class DeclarationMapperSpec extends PlaySpec
+  with OneAppPerSuite
+  with ScalaDataExamples
+  with DesScalaExamples {
+
+  val output = DeclarationMapper.toDomain(desDeclaration,date, true)
+
+  "Declaration Mapper" must {
+    "Map fields correctly to a Domain declaration" when {
+      "we have a correspondence address" in {
+          output.correspondenceAddress.postalCode mustBe desDeclaration.address.postCode
+      }
+
+      "we have a firstName" in {
+        output.givenName mustBe desDeclaration.name.firstName
+      }
+
+      "we have a middle name" in {
+        output.otherName mustBe desDeclaration.name.middleName
+      }
+
+      "we have a family name" in {
+        output.familyName mustBe desDeclaration.name.lastName
+      }
+
+      "we have a date" in {
+        output.date mustBe date
+      }
+
+      "we have a confirmation" in {
+        output.confirmation mustBe true
+      }
+    }
+  }
+}
+
+object DeclarationMapper extends DesScalaExamples{
+  def toDomain(declaration: DesDeclaration, date: DateTime, confirmation: Boolean) : Declaration = {
+    Declaration(AddressMapper.toDomain(declaration.address),confirmation,declaration.name.firstName,declaration.name.lastName,date,declaration.name.middleName)
+  }
+}
