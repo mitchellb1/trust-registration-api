c7dc5a18db05f418836ea6895933540586826567
David Wright <davew@MacBook-Pro.local>
2017-06-08 10:31:35 +0100

    Removed play-url binders and updated versions of other libraries

--- project/ApiServiceBuild.scala
+++ project/ApiServiceBuild.scala
@@ -18,31 +18,32 @@ private object AppDependencies {
   import play.core.PlayVersion
   import play.sbt.PlayImport._
 
-  private val microserviceBootstrapVersion = "5.5.0"
-  private val playHealthVersion = "2.0.0"
-  private val playAuthVersion = "4.2.0"
-  private val playUrlBindersVersion = "2.0.0"
-  private val playJsonLoggerVersion = "3.0.0"
-  private val playConfigVersion = "3.0.0"
-  private val hmrcTestVersion = "2.0.0"
-  private val playReactivemongoVersion = "5.1.0"
+  private val microserviceBootstrapVersion = "5.14.0"
+  private val playHealthVersion = "2.1.0"
+  private val playAuthVersion = "4.3.0"
+  //private val playUrlBindersVersion = "2.0.0"
+  private val playJsonLoggerVersion = "3.1.0"
+  private val playConfigVersion = "4.3.0"
+  private val hmrcTestVersion = "2.3.0"
+//  private val playReactivemongoVersion = "5.1.0"
   private val domainVersion = "4.0.0"
 
   private val playHmrcApiVersion = "0.5.0"
 
   private val scalaTestVersion = "2.2.6"
   private val scalaTestPlusVersion = "1.5.1"
-  private val mockitoVersion = "1.9.5"
-  private val wireMockVersion = "1.57"
+  private val mockitoVersion = "1.10.19"
+  private val wireMockVersion = "1.58"
+  private val pegDownVersion = "1.6.0"
 
   val compile = Seq(
-    "uk.gov.hmrc" %% "play-reactivemongo" % playReactivemongoVersion,
+   // "uk.gov.hmrc" %% "play-reactivemongo" % playReactivemongoVersion,
 
     ws,
     "uk.gov.hmrc" %% "microservice-bootstrap" % microserviceBootstrapVersion,
     "uk.gov.hmrc" %% "play-authorisation" % playAuthVersion,
     "uk.gov.hmrc" %% "play-health" % playHealthVersion,
-    "uk.gov.hmrc" %% "play-url-binders" % playUrlBindersVersion,
+
     "uk.gov.hmrc" %% "play-config" % playConfigVersion,
     "uk.gov.hmrc" %% "play-json-logger" % playJsonLoggerVersion,
     "uk.gov.hmrc" %% "domain" % domainVersion,
@@ -50,7 +51,7 @@ private object AppDependencies {
     "com.github.tomakehurst" % "wiremock" % wireMockVersion % "test,it",
     "com.github.fge" % "json-schema-validator" % "2.2.6"
   )
-
+ // "uk.gov.hmrc" %% "play-url-binders" % playUrlBindersVersion,
   trait TestDependencies {
     lazy val scope: String = "test"
     lazy val test: Seq[ModuleID] = ???
@@ -62,7 +63,7 @@ private object AppDependencies {
         "uk.gov.hmrc" %% "hmrctest" % hmrcTestVersion % scope,
         "org.scalatest" %% "scalatest" % scalaTestVersion % scope,
         "org.mockito" % "mockito-core" % mockitoVersion % scope,
-        "org.pegdown" % "pegdown" % "1.5.0" % scope,
+        "org.pegdown" % "pegdown" % pegDownVersion % scope,
         "com.typesafe.play" %% "play-test" % PlayVersion.current % scope,
         "org.scalatestplus.play" %% "scalatestplus-play" % scalaTestPlusVersion % scope
       )
